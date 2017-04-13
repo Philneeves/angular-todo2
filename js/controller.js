3c07d1177cbdb99df772c8df15fc74529b5fc1ff
Philneeves <p.neeves@gmail.com>
2017-04-13 11:43:01 +0100

    testing out angular bootstrap patch for menu collapse

--- js/controller.js
+++ js/controller.js
@@ -130,3 +130,9 @@ angular.module('RouteControllers', [])
 			}
 		}
 	});
+
+	angular.module('ui.bootstrap.demo').controller('CollapseDemoCtrl', function ($scope) {
+  $scope.isNavCollapsed = true;
+  $scope.isCollapsed = false;
+  $scope.isCollapsedHorizontal = false;
+});
